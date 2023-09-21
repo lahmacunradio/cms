adce0adf55da1a473c7376fc4a05abb3e92e924d
gammaw <bokorpet@gmail.com>
2023-09-21 09:32:09 +0200

    Add secrets template file

---
+++ wp/wp-content/plugins/stripe-for-lahmacun/secrets.template.php
@@ -0,0 +1,7 @@
+<?php
+// Keep your Stripe API key protected by including it as an environment variable
+// or in a private script that does not publicly expose the source code.
+$stripeSecretKey = 'ADD_API_KEY';
+$YOUR_DOMAIN = 'http://localhost';
+$success_url = '/thanks-for-your-payment';
+$cancel_url = '/thanks-for-your-payment';
