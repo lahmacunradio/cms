6dd393f5fca77f27dc56bcd6ca5ba0efe03505da
gammaw <bokorpet@gmail.com>
2023-09-21 09:29:36 +0200

    Add basic custom API through custom plugin

---
+++ wp/wp-content/plugins/stripe-for-lahmacun/stripe-for-lahmacun.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ * Plugin Name: Stripe for Lahmacun
+ */
+
+require_once 'stripe/init.php';
+require_once 'secrets.php';
+
+
+function my_register_route() {
+    register_rest_route('my-route', 'my-phrase', array(
+        'methods' => 'GET',
+        'callback' => 'custom_phrase',
+            )
+    );
+}
+function custom_phrase() {
+    global $stripeSecretKey;
+    \Stripe\Stripe::setApiKey($stripeSecretKey);
+
+    header('Content-Type: application/json');
+
+    global $YOUR_DOMAIN;
+    global $success_url;
+    global $cancel_url;
+    
+    $checkout_session = \Stripe\Checkout\Session::create([
+      'line_items' => [[
+        # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
+        'price' => 'price_1Nsh2UBd0rYu9XiDS0kGER2h',
+        'quantity' => 1,
+      ]],
+      'mode' => 'subscription',
+      'success_url' => $YOUR_DOMAIN . $success_url,
+      'cancel_url' => $YOUR_DOMAIN . $cancel_url,
+      'subscription_data' => [
+        'metadata' => [
+            'lahmacun_form' => 'recurring_membership',
+        ]
+      ]
+    ]);
+    
+    header("HTTP/1.1 303 See Other");
+    header("Location: " . $checkout_session->url);
+
+//    return new WP_REST_Response('Hello World! This is my first REST API');
+}
+
+add_action('rest_api_init', 'my_register_route');
